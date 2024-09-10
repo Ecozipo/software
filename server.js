224adac1106ebbefd30fd0c86e9916000eb093da
ValicioKely <razalicio008@gmail.com>
2024-09-10 07:47:51 +0300

    manage devices

--- server.js
+++ server.js
@@ -18,19 +18,68 @@ const device = awsIot.device({
   keyPath: "credentials/81e5e6a3a666ef15c189137f854e262068c25f1af24258a82c73a56e15ce7800-private.pem.key",
   certPath: 'credentials/81e5e6a3a666ef15c189137f854e262068c25f1af24258a82c73a56e15ce7800-certificate.pem.crt',
   caPath: 'credentials/AmazonRootCA1.pem',
-  clientId: 'Node',
+  clientId: 'client',
   host: 'a27g25yfuax5ui-ats.iot.us-east-1.amazonaws.com'
 });
 
 device.on('connect', function() {
   console.log('Connected to AWS IoT Core');
-  // After connecting, you may want to publish/subscribe to topics
+
+  /*
+  
+  
+    SUBSCRIBE TO THE STATE OF ALL LED 
+    THE PAYLOAD WILL BE 
+    {
+      "state": {
+        "desired": {
+          "welcome": "aws-iot",
+          "relay_1": 1,
+          "relay_2": 1,
+          "relay_3": 1,
+          "relay_4": 1,
+          "relay_5": 1,
+          "relay_6": 1,
+          "relay_7": 1,
+          "relay_8": 0
+        },
+        "reported": {
+          "welcome": "aws-iot",
+          "relay_1": 1,
+          "relay_2": 1,
+          "relay_3": 1,
+          "relay_4": 1,
+          "relay_5": 1,
+          "relay_6": 1,
+          "relay_7": 1,
+          "relay_8": 0
+        }
+      }
+    }
+
+    REPORTED AND DESIRED WILL BE THE SAME IF THE OBJECT IS CONNECTED TO INTERNET
+  */
+  device.subscribe('$aws/things/Spare/shadow/get/accepted',(err,paylod,hafa)=>{
+    if(err)console.log(err)
+    console.log(paylod)
+    device.emit("message","accepted",JSON.stringify(paylod))
+  });
+
+  /*
+
+    THE MEASUREMENT OF PZEM004T SENSOR 
+    THE RESPONSE WILL BE 
+
+  
+
+  */
+
   device.subscribe('esp32/pzem',(err,paylod,hafa)=>{
     if(err)console.log(err)
     console.log(paylod)
-    device.emit("message","esp32/pzem",JSON.stringify(paylod))
+    device.emit("message","accepted",JSON.stringify(paylod))
   });
-  device.publish('esp32/pub', JSON.stringify({ key: 'value' }));
+
 });
 
 device.on('message', function(topic, payload) {
@@ -40,7 +89,7 @@ device.on('message', function(topic, payload) {
 app.post('/publish', (req, res) => {
     const message = req.body;
   
-    device.publish('esp32/led', JSON.stringify(message), (err) => {
+    device.publish('$aws/things/Spare/shadow/update', JSON.stringify(message), (err) => {
       if (err) {
         console.error('Error publishing message:', err);
         res.status(500).send('Error publishing message');
